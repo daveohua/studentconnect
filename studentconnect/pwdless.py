8e251ffcdcfda139e3c3b76af501e31a0484d43e
David Ohuabunwa <mail@davidohuabunwa.net>
2026-02-19 02:20:52 +0000

    made SMTP server optional: if credentials are in env file use SMTP server otherwise print auth token to console; pin PyJWT due to breaking change

--- studentconnect/pwdless.py
+++ studentconnect/pwdless.py
@@ -1,11 +1,18 @@
 import jwt
 import email
+import os
 import smtplib
 from jinja2 import Environment, PackageLoader
+from dotenv import load_dotenv
 from datetime import datetime
 
-env = Environment(loader = PackageLoader("studentconnect"))
+load_dotenv()
+SMTP_SERVER = os.getenv("SC_SMTP_SERVER")
+SMTP_PORT = os.getenv("SC_SMTP_PORT")
+SMTP_USERNAME = os.getenv("SC_SMTP_USERNAME")
+SMTP_PASSWORD = os.getenv("SC_SMTP_PASSWORD")
 
+env = Environment(loader = PackageLoader("studentconnect"))
 
 def generate_token(user):
     payload = {
@@ -28,23 +35,25 @@ def verify_token(token, db):
 def send_token(token, user, base_url):
     token_url = '?token='.join([base_url, token])
 
-    details = {
-        'name': user['FirstName'],
-        'token_url': token_url
-    }
+    if SMTP_SERVER is not None:
+        details = {
+            'name': user['FirstName'],
+            'token_url': token_url
+        }
 
-    msg = email.message.EmailMessage()
-    msg['Subject'] = "Magic link"
-    msg['From'] = email.headerregistry.Address(
-        "Student Connect", "studentconnect", "davidohuabunwa.net"
-        )
-    msg['To'] = email.headerregistry.Address(addr_spec=user['email'])
-    msg.set_content(env.get_template("pwdless/email.txt").render(details))
-    msg.add_alternative(env.get_template("pwdless/email.html").render(details), subtype='html')
+        msg = email.message.EmailMessage()
+        msg['Subject'] = "Magic link"
+        msg['From'] = email.headerregistry.Address(
+            "Student Connect", "studentconnect", "davidohuabunwa.net"
+            )
+        msg['To'] = email.headerregistry.Address(addr_spec=user['email'])
+        msg.set_content(env.get_template("pwdless/email.txt").render(details))
+        msg.add_alternative(env.get_template("pwdless/email.html").render(details), subtype='html')
 
-    server = smtplib.SMTP_SSL("smtp.sendgrid.net", 465)
-    server.login("apikey", "secret")
-    server.send_message(msg)
-    server.quit()
+        server = smtplib.SMTP_SSL("smtp.sendgrid.net", 465)
+        server.login("apikey", "secret")
 
-    print(token)
+        server.send_message(msg)
+        server.quit()
+
+    print(token_url)
